d6221024162e2c4d47bcf2a1c02fffdc1533baeb
Shane Wilson <shanezilla@gmail.com>
2021-01-29 21:44:51 -0500

    🎉

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:react/recommended",
+  ],
+  plugins: ["react-hooks"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
